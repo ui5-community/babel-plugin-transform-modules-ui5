db73ebca6f7068198117778390191b9147a63856
Ryan Murphy <ryan.murphy@technitask.com>
2017-09-29 15:38:09 -0400

    tests: Create the result file right away

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-/* global it, expect, afterAll */
+/* global it, expect */
 const fse = require('fs-extra')
 const Path = require('path')
 const babel = require('babel-core')
 const plugin = require('..')
 
-const fixtureDirPath = Path.resolve(__dirname, "fixtures")
-const resultsDir = Path.join(__dirname, "output")
+const fixtureDirPath = Path.resolve(__dirname, 'fixtures')
+const resultsDir = Path.join(__dirname, 'output')
 
 fse
   .emptyDirSync(resultsDir)
@@ -13,37 +13,19 @@ fse
 fse
   .readdirSync(fixtureDirPath)
   .filter(item => item.endsWith('.js'))
-  .forEach(item => {
-    const filename = Path.join(fixtureDirPath, item)
+  .forEach(filename => {
+    const filepath = Path.join(fixtureDirPath, filename)
     const babelOpts = {
       plugins: [
-        "babel-plugin-syntax-decorators",
-        "babel-plugin-syntax-class-properties",
+        'babel-plugin-syntax-decorators',
+        'babel-plugin-syntax-class-properties',
         plugin
       ]
     }
-    const result = babel.transformFileSync(filename, babelOpts).code
-    it(item, () => {
+    const result = babel.transformFileSync(filepath, babelOpts).code
+    const resultFilepath = Path.join(resultsDir, filename)
+    fse.writeFileSync(resultFilepath, result)
+    it(filename, () => {
       expect(result).toMatchSnapshot()
     })
   })
-
-afterAll(() => {
-  // Log the results into individual files. These are for manual spot-checking, not for automated tests.
-  try {
-    const sciptName = Path.basename(__filename)
-    const snapshots = require(Path.join(__dirname, '__snapshots__', `${sciptName}.snap`))
-    Object.keys(snapshots).forEach(item => {
-      const filename = item.split('.js')[0] + '.js'
-      const filepath = Path.join(resultsDir, filename)
-      const escapedCode = snapshots[item]
-      const plainCode = escapedCode
-        .trim()
-        .slice(1, -1) // Remove the surrounding quotes
-        .replace(/\\"/g, '"')
-      fse.writeFileSync(filepath, plainCode)
-    })
-  } catch (error) {
-    console.error(error)
-  }
-})
