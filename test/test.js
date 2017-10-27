1984dc053ccc11a92e35ca437a9a7e85bf13d62b
Ryan Murphy <ryan.murphy@technitask.com>
2017-10-26 23:16:55 -0400

    test refactoring (an re-enable snapshots)

--- test/test.js
+++ test/test.js
@@ -1,30 +1,53 @@
-/* global test, expect */
+/* global test, expect, describe */
 const fse = require('fs-extra')
 const Path = require('path')
 const babel = require('babel-core')
 const plugin = require('..')
 
-const fixtureDirPath = Path.resolve(__dirname, 'fixtures')
 const outputDir = Path.join(__dirname, 'output')
+fse.emptyDirSync(outputDir)
 
-fse
-  .emptyDirSync(outputDir)
+function main() {
+  const fixtureDirPath = Path.resolve(__dirname, 'fixtures')
+  processDirectory(fixtureDirPath)
+}
 
-fse
-  .readdirSync(fixtureDirPath)
-  .filter(item => item.endsWith('.js'))
-  .forEach(filename => {
-    test(filename, () => {
-      console.log(`Running ${filename}`) // eslint-disable-line
-      const filepath = Path.join(fixtureDirPath, filename)
-      const result = babel.transformFileSync(filepath, {
-        plugins: [
-          'babel-plugin-syntax-decorators',
-          'babel-plugin-syntax-class-properties',
-          plugin
-        ]
-      }).code
-      fse.writeFileSync(Path.join(outputDir, filename), result) // For manual verification
-      // expect(result).toMatchSnapshot()
+function processDirectory(dir) {
+  const items = fse.readdirSync(dir)
+  // Process Files first
+  items
+    .filter(item => item.endsWith('.js'))
+    .forEach(filename => {
+      test(filename, () => {
+        console.log(`Running ${filename}`) // eslint-disable-line
+        const filepath = Path.join(dir, filename)
+        const result = babel.transformFileSync(filepath, {
+          plugins: [
+            'babel-plugin-syntax-decorators',
+            'babel-plugin-syntax-class-properties',
+            plugin
+          ]
+        }).code
+        fse.writeFileSync(Path.join(outputDir, filename), result) // For manual verification
+        expect(result).toMatchSnapshot()
+      })
     })
-  })
+    // Recurse into directories
+  items
+    .map(name => {
+      const path = Path.join(dir, name)
+      return {
+        name,
+        path,
+        stat: fse.statSync(path)
+      }
+    })
+    .filter(item => item.stat.isDirectory())
+    .forEach((item) => {
+      describe(item.name, () => {
+        processDirectory(item.path)
+      })
+    })
+}
+
+main()
