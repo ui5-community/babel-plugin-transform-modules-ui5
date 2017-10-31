15f8447339ac0fb465825f126f30dac07db812fb
Ryan Murphy <ryan.murphy@technitask.com>
2017-10-31 14:21:35 -0400

    supprt for correcting metadata and renderer properties

---
+++ test/fixtures/classes/class-convert-metadata-jsdoc.js
@@ -0,0 +1,10 @@
+
+import SAPClass from 'sap/SAPClass'
+
+const mdata = {}
+
+/**
+ * @metadata mdata
+ */
+export default class MyClass extends SAPClass {
+}
---
+++ test/fixtures/classes/class-convert-metadata-static.js
@@ -0,0 +1,8 @@
+
+import SAPClass from 'sap/SAPClass'
+
+const mdata = {}
+
+export default class MyClass extends SAPClass {
+  static metadata = mdata;
+}
