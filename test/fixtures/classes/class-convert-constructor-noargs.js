587945a7ab27543573c7a06fe126ed56d3270b17
Ryan Murphy <ryan.murphy@technitask.com>
2017-11-08 22:46:08 -0500

    use .call instead of .apply for super updates

---
+++ test/fixtures/classes/class-convert-constructor-noargs.js
@@ -0,0 +1,15 @@
+/* global sap */
+/**
+ * Test notes: Even though there is an existing sp.ui.define,
+ * the class should be converted.
+ */
+sap.ui.define(['sap/SAPClass'], (SAPClass) => {
+  /**
+   * @name com.app.MyClass
+   */
+  return class MyClass extends SAPClass {
+    constructor() {
+      super()
+    }
+  }
+})
